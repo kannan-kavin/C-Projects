24b5bd7a59dab91ff55d73dead09b2c3228a5de6
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-09-22 18:02:51 +0000

    Fri, Sep 22, 2023, 2:02 PM -04:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -10,7 +10,7 @@
 int main(void)
 {
        float C = get_float("Change owed: $");
-        while (C > 0);
+        while (C < 0);
        float T = ((C * 100) / 25);
        printf("The total number of coins in change you need is %F\n", C);
 }
