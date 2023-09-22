2d98a43bf45e3e40cde7ca9cf825493f06f05144
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-09-22 15:38:33 +0000

    Fri, Sep 22, 2023, 11:38 AM -04:00

---
+++ chapter1/cash/cash.c
@@ -0,0 +1,16 @@
+/*
+ * cash.c by Kavin Kannan
+ * 9/20/2023
+ * Calculate least number of coins to make change
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+       float change = get_float("Change owed: ");
+        while (change > 0);
+       float total =  * 100 / 25;
+       printf("The total number of coins in change you need is %f\n", total);
+}
--- chapter1/fahrenheit/fahrenheit.c
+++ chapter1/fahrenheit/fahrenheit.c
@@ -4,6 +4,6 @@
 int main(void)
 {
     float C = get_float("Temperature in Celcius: ");
-    float F = ((C*9)/5)+32;
+    float F = ((C * 9) / 5) + 32;
     printf("The Temperature in Fahrenheit is %.1F°\n", F);
 }
