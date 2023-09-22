e1c833e4e3e78b5e2ab0002560d638eee8e77b20
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-09-22 18:17:29 +0000

    Fri, Sep 22, 2023, 2:17 PM -04:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -12,8 +12,11 @@ int main(void)
        float C = get_float("Change owed: $");
         while (C < 0);
        float Q = ((C * 100) / 25);
-       int R = (((C * 100) % 25));
+       int R = ((C * 100) % 25);
        float D = (R / 10);
-       int R = 
-       printf("The total number of coins in change you need is %F\n", T);
+       int R = (R % 10);
+       float N = (R / 5);
+       int P = (R % 5);
+       int A = Q + D + N + P;
+       printf("The total number of coins in change you need is %.0i\n", A);
 }
