0b48b03cd8e27d95e6b9e24561263a62547e03a4
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-09-22 18:21:19 +0000

    Fri, Sep 22, 2023, 2:21 PM -04:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -12,7 +12,8 @@ int main(void)
        float C = get_float("Change owed: $");
         while (C < 0);
        float Q = ((C * 100) / 25);
-       int R = ((C * 100) % 25);
+       int help = (C * 100);
+       int R = help % 25;
        float D = (R / 10);
        int R = (R % 10);
        float N = (R / 5);
