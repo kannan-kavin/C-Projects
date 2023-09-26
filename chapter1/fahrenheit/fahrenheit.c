b592707a20a9bf5568f83c1a167383ffe065f4f9
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-09-26 16:36:34 +0000

    Tue, Sep 26, 2023, 12:36 PM -04:00

--- chapter1/fahrenheit/fahrenheit.c
+++ chapter1/fahrenheit/fahrenheit.c
@@ -4,6 +4,8 @@
 int main(void)
 {
     float C = get_float("Temperature in Celcius: ");
+    // C represents the Temperature in Celcius that is inputted
     float F = ((C * 9) / 5) + 32;
+    // F represents the Temperature in Fahrenheit that is returned
     printf("The Temperature in Fahrenheit is %.1F°\n", F);
 }
