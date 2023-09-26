fb8e74f814df238521c1e7cde2af2629d9f323d6
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-09-26 15:59:57 +0000

    Tue, Sep 26, 2023, 11:59 AM -04:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -10,13 +10,18 @@
 int main(void)
 {
        float C = get_float("Change owed: $");
-       // C is Change inputed by user
         while (C < 0);
-       float Q = ((C * 100) / 25);
-       int help = (C * 100);
+       // C is Change inputed by user
+       int helper = (C * 100);
+       // helper is a variable which turns decimal inputs into a whole number
+       float Q = (helper / 25);
+       // Q is Max Quarters needed
        int R1 = help % 25;
+       // R1 is the remainder of change in cents still needed after we've taken out all of our Quarters
        float D = (R1 / 10);
+       // D is Max Dimes needed
        int R2 = (R1 % 10);
+       // R2 is the remainder of change in cents still needed after we've taken out all of our Quarters AND Dimes
        float N = (R2 / 5);
        int P = (R2 % 5);
        int A = Q + D + N + P;
