a9aca185942c18c85414725d446a09a10785a45a
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-09-27 14:00:13 +0000

    Wed, Sep 27, 2023, 10:00 AM -04:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -14,13 +14,14 @@ int main(void)
         ;
     // C is Change inputed by user
     float helper = (C * 100);
-    helper = (int)helper;
+    int inthelp;
+    inthelp = (int)helper;
     printf("%f\n", helper);
     // helper is a variable which turns decimal inputs into a whole number
-    int Q = (helper / 25);
+    int Q = (inthelp / 25);
     // Q is Max Quarters needed
     printf("%i\n", Q);
-    int R1 = helper % 25;
+    int R1 = inthelp % 25;
     // R1 is the remainder of change in cents still needed after we've taken out all of our Quarters
     int D = (R1 / 10);
     // D is Max Dimes needed
