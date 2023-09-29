f51ad8b902ecfcd0ff59e1db0f160c4d3e895d05
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-09-29 18:07:36 +0000

    Fri, Sep 29, 2023, 2:07 PM -04:00

--- chapter1/population/population.c
+++ chapter1/population/population.c
@@ -5,5 +5,8 @@ int main(void)
 {
     int Start = get_int("Starting population: ");
     int End = get_int("Ending population: ");
-    
+    while(9<Start<End)
+        ;
+    printf("%i\n", Start);
+    printf("%i\n", End);
 }
