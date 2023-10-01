b59fb25aa821e80300899450af695c5a300a4572
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-01 22:16:53 +0000

    Sun, Oct 1, 2023, 6:16 PM -04:00

--- chapter1/population/population.c
+++ chapter1/population/population.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
     int Start = get_int("Starting Population: ");
+    do
+    {
+        int End = get_int("Ending Population: ");
         do
         {
-            int End = get_int("Ending Population: ")
-                do
-                {
-                    int Now = (Start + (Start/3) - (Start/4))
-                }
-                while (End > Start);
+            int Now = (Start + (Start / 3) - (Start / 4));
         }
-        while (Start > 9);
-  }
+        while (End > Start);
+    }
+    while (Start > 9);
+}
