1b124c21a4d165c5660a984b4c2b640df64776c6
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-01 14:53:29 +0000

    Sun, Oct 1, 2023, 10:53 AM -04:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -11,19 +11,26 @@
 int main(void)
 {
     float change;
-    
+
     do
     {
         change = get_float("Change owed: ");
     }
     while (change < 0)
-
-    // TODO - Convert (and round) to cents/number of pennies
-
-    // TODO - Quarters
-    // TODO - Dimes
-    // TODO - Nickels
-    // TODO - Pennies
-
-    // TODO - Report
-}
+// C is Change inputed by user
+    int helper = round(C * 100);
+    // helper is a variable which turns decimal inputs into a whole number
+    int Q = (helper / 25);
+    // Q is Max Quarters needed
+    int R1 = helper % 25;
+    // R1 is the remainder of change in cents still needed after we've taken out all of our Quarters
+    int D = (R1 / 10);
+    // D is Max Dimes needed
+    int R2 = (R1 % 10);
+    // R2 is the remainder of change in cents still needed after we've taken out all of our Quarters AND Dimes
+    int N = (R2 / 5);
+    // N is Max Nickles needed
+    int P = (R2 % 5);
+    // P is the remainder of change in cents still needed after we've taken out all of our Quarters AND Dimes AND Nickles
+    int S = Q + D + N + P;
+    // S is the sum of all of our max coins needed in each category, returning the answer of max coins
