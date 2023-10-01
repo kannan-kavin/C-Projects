a972b9f8a6f19d6e9cd04885f85196f7e32344b0
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-01 14:45:17 +0000

    Sun, Oct 1, 2023, 10:45 AM -04:00

---
+++ chapter1/cash/cash.c
@@ -0,0 +1,28 @@
+/*
+ * cash.c by YOUR NAME HERE
+ * 9/23/2021
+ * Calculate least number of coins to make change
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user for non-negative amount of change owed
+    float change;
+    do
+    {
+        change = get_float("Change owed: ");
+    }
+    while (change < 0)
+
+    // TODO - Convert (and round) to cents/number of pennies
+
+    // TODO - Quarters
+    // TODO - Dimes
+    // TODO - Nickels
+    // TODO - Pennies
+
+    // TODO - Report
+}
