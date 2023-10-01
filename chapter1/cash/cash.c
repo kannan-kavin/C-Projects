4b30427721f8f49d2f9d3ffd1dc2335ea818c2c7
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-01 14:57:36 +0000

    Sun, Oct 1, 2023, 10:57 AM -04:00

--- chapter1/cash/cash.c
+++ chapter1/cash/cash.c
@@ -17,21 +17,21 @@ int main(void)
     }
     while (change < 0);
 // C is Change inputed by user
-    int helper = round(C * 100);
+    int helper = round(change * 100);
     // helper is a variable which turns decimal inputs into a whole number
-    int Q = (helper / 25);
-    // Q is Max Quarters needed
-    int R1 = helper % 25;
-    // R1 is the remainder of change in cents still needed after we've taken out all of our Quarters
-    int D = (R1 / 10);
-    // D is Max Dimes needed
-    int R2 = (R1 % 10);
-    // R2 is the remainder of change in cents still needed after we've taken out all of our Quarters AND Dimes
-    int N = (R2 / 5);
-    // N is Max Nickles needed
-    int P = (R2 % 5);
-    // P is the remainder of change in cents still needed after we've taken out all of our Quarters AND Dimes AND Nickles
-    int S = Q + D + N + P;
-    // S is the sum of all of our max coins needed in each category, returning the answer of max coins
-    printf("The total number of coins in change you need is %.0i\n", S);
+    int quarter = (helper / 25);
+    // quarter is Max Quarters needed
+    int remainder1 = helper % 25;
+    // remainder1 is the remainder of change in cents still needed after we've taken out all of our Quarters
+    int dime = (remainder1 / 10);
+    // dime is Max Dimes needed
+    int remainder2 = (remainder1 % 10);
+    // remainder2 is the remainder of change in cents still needed after we've taken out all of our Quarters AND Dimes
+    int nickle = (remainder2 / 5);
+    // nickle is Max Nickles needed
+    int penny = (remainder2 % 5);
+    // penny is the remainder of change in cents still needed after we've taken out all of our Quarters AND Dimes AND Nickles
+    int sum = quarter + dime + nickle + penny;
+    // sum is the sum of all of our max coins needed in each category, returning the answer of max coins
+    printf("The total number of coins in change you need is %.0i\n", sum);
 }
