306be60d17e574a6625d984a18d90fe1fbc45886
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-06 18:14:03 +0000

    Fri, Oct 6, 2023, 2:14 PM -04:00

--- chapter1/mario/mario.c
+++ chapter1/mario/mario.c
@@ -10,5 +10,17 @@ int main(void)
         height = get_int("Height: ");
     }
     while (height > 8 || height < 1);
-    printf("Height: %i\n", height);
+    // printf("Height: %i\n", height);
+    int i;
+    for (i = 0; i < height; i++)
+    {
+        if (i == height)
+        {
+            printf("#");
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
 }
