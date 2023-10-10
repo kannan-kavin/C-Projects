bb3f3abd3786c157852b2857d09cad60b49ffd7c
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-10 16:23:28 +0000

    Tue, Oct 10, 2023, 12:23 PM -04:00

--- chapter1/mario/mario.c
+++ chapter1/mario/mario.c
@@ -17,7 +17,7 @@ int main(void)
     // used to calculate hashes per row
     for (i = 1; i <= height; i++)
     {
-        for (j = 1; j > 0; j--)
+        for (j = (height - 1); j > 0; j--)
         {
         if(j>0)
             {
@@ -28,7 +28,7 @@ int main(void)
                 printf("#");
             }
             // printf("\n", i);
-        printf("\n");
         }
+        printf("\n");
     }
 }
