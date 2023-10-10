5e6e06b3dcb46138cc8b6a212558d351229f2697
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-10 16:14:11 +0000

    Tue, Oct 10, 2023, 12:14 PM -04:00

--- chapter1/mario/mario.c
+++ chapter1/mario/mario.c
@@ -15,7 +15,7 @@ int main(void)
     // used to calculate spaces per row
     int j;
     // used to calculate hashes per row
-    for (i = 1; i <= height, i++)
+    for (i = 1; i <= height; i++)
     {
         for (j = 1; j > 0; j++)
         {
