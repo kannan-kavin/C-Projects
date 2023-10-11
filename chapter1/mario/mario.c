52beb3eb268f7508a6f603a249d67099f9e54d91
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-11 19:39:45 +0000

    Wed, Oct 11, 2023, 3:39 PM -04:00

--- chapter1/mario/mario.c
+++ chapter1/mario/mario.c
@@ -28,7 +28,7 @@ int main(void)
         {
             printf("#");
         }
-        printf("   ");
+        printf("  ");
         for (k = 0; k <= (height - j); k++)
         {
             printf("#");
