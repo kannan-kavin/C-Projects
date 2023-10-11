886926f3ba1eb58451ca8d56096e34ec70417f20
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-11 18:37:28 +0000

    Wed, Oct 11, 2023, 2:37 PM -04:00

--- chapter1/mario/mario.c
+++ chapter1/mario/mario.c
@@ -10,12 +10,14 @@ int main(void)
     }
     while (height > 8 || height < 1);
     // printf("Height: %i\n", height);
+    // used to get an input between 1 and 8 inclusive for our height
 
     int i;
-    // used to calculate spaces per row
+    // used to separate each row from the next
     int j;
     // used to calculate hashes per row
     int k;
+    // used to calculate spaces per row
     for (i = 0; i < height; i++)
     {
         for (j = 1; j < (height - i); j++)
@@ -23,7 +25,7 @@ int main(void)
            printf("-");
         }
         // I DONT KNOW WHATS HAPPENING HERE
-        for (k = 0; k == height; k++)
+        for (k = 0; k <= (height-j); k++)
         {
             printf("#");
         }
