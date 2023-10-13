5911511b4c120cb4db0298ed0695b8ec38fa67dc
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-13 17:53:33 +0000

    Fri, Oct 13, 2023, 1:53 PM -04:00

--- chapter1/credit/credit.c
+++ chapter1/credit/credit.c
@@ -7,15 +7,15 @@ int main(void)
     CCN = get_long("Enter Credit Card Number: ");
     if ((CCN / 1000000000000000 > 1))
     {
-        printf("MASTERCARD OR VISA\n");
+        //printf("MASTERCARD OR VISA\n");
     }
     else if ((CCN / 100000000000000) > 1)
     {
-        printf("AMERICAN EXPRESS\n");
+        //printf("AMERICAN EXPRESS\n");
     }
     else if ((CCN / 1000000000000) > 1)
     {
-        printf("VISA\n");
+        //printf("VISA\n");
     }
     else
     {
