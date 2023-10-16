eaef09562fb29f65aeed54817cd37827696b7b5c
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-16 15:32:24 +0000

    Mon, Oct 16, 2023, 11:32 AM -04:00

--- chapter1/credit/credit.c
+++ chapter1/credit/credit.c
@@ -17,16 +17,23 @@ int main(void)
             //printf("VISA\n");
             int c1 = (floor(CNN % 10) / 1);
             int c2 = 2 * (floor(CCN % 100) / 10);
+            int c3 = (floor(CNN % 1000) / 100);
             int c4 = 2 * (floor(CCN % 10000) / 1000);
+            int c5 = (floor(CNN % 100000) / 10000);
             int c6 = 2 * (floor(CCN % 1000000) / 100000);
+            int c7 = (floor(CNN % 10000000) / 1000000);
             int c8 = 2 * (floor(CCN % 100000000) / 10000000);
+            int c9 = (floor(CNN % 1000000000) / 100000000);
             int c10 = 2 * (floor(CCN % 10000000000) / 1000000000);
+            int c11 = (floor(CNN % 100000000000) / 10000000000);
             int c12 = 2 * (floor(CCN % 1000000000000) / 100000000000);
+            int c13 = (floor(CNN % 10000000000000) / 1000000000000);
             int c14 = 2 * (floor(CCN % 100000000000000) / 10000000000000);
+            int c15 = (floor(CNN % 1000000000000000) / 100000000000000);
             int c16 = 2 * (floor(CCN % 10000000000000000) / 1000000000000000);
             //printf("Each one is: %i, %i, %i, %i, %i, %i, %i, %i\n", check1, check2, check3, check4, check5, check6, check7, check8);
-            int add = ((c2 / 10) + (c2 % 10) + (c4 / 10) + (c4 % 10) + (c6 / 10) + (c6 % 10) + (c8 / 10) + (c8 % 10) + (c10 / 10) + (c10 % 10) + (c12 / 10) + (c12 % 10) + (c14 / 10) + (c14 % 10) + (c16 / 10) + (c16 % 10))
-
+            int step1 = ((c2 / 10) + (c2 % 10) + (c4 / 10) + (c4 % 10) + (c6 / 10) + (c6 % 10) + (c8 / 10) + (c8 % 10) + (c10 / 10) + (c10 % 10) + (c12 / 10) + (c12 % 10) + (c14 / 10) + (c14 % 10) + (c16 / 10) + (c16 % 10))
+            int step2 = 
         }
         else if (MASTERCARD16 == 51 || MASTERCARD16 == 52 || MASTERCARD16 == 53 || MASTERCARD16 == 54 || MASTERCARD16 == 55)
         {
