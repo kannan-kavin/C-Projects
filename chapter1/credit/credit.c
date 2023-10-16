b3bc0505c187c544825ae5880cb66e574c7e2e46
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-16 15:25:25 +0000

    Mon, Oct 16, 2023, 11:25 AM -04:00

--- chapter1/credit/credit.c
+++ chapter1/credit/credit.c
@@ -15,7 +15,6 @@ int main(void)
         if (VISA16 == 4)
         {
             //printf("VISA\n");
-            //
             int c1 = 2 * (floor(CCN % 100) / 10);
             int c2 = 2 * (floor(CCN % 10000) / 1000);
             int c3 = 2 * (floor(CCN % 1000000) / 100000);
@@ -26,7 +25,7 @@ int main(void)
             int c8 = 2 * (floor(CCN % 10000000000000000) / 1000000000000000);
             //printf("Each one is: %i, %i, %i, %i, %i, %i, %i, %i\n", check1, check2, check3, check4, check5, check6, check7, check8);
             int add = ((c1 / 10) + (c1 % 10) + (c2 / 10) + (c2 % 10) + (c3 / 10) + (c3 % 10) + (c4 / 10) + (c4 % 10) + (c5 / 10) + (c5 % 10) + (c6 / 10) + (c6 % 10) + (c7 / 10) + (c7 % 10) + (c8 / 10) + (c8 % 10))
-            
+
         }
         else if (MASTERCARD16 == 51 || MASTERCARD16 == 52 || MASTERCARD16 == 53 || MASTERCARD16 == 54 || MASTERCARD16 == 55)
         {
