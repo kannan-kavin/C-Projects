357c4e3be39e68f5ba2425fc461b88e6cd15d4b3
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-16 15:29:14 +0000

    Mon, Oct 16, 2023, 11:29 AM -04:00

--- chapter1/credit/credit.c
+++ chapter1/credit/credit.c
@@ -15,16 +15,17 @@ int main(void)
         if (VISA16 == 4)
         {
             //printf("VISA\n");
-            int c1 = 2 * (floor(CCN % 100) / 10);
-            int c2 = 2 * (floor(CCN % 10000) / 1000);
-            int c3 = 2 * (floor(CCN % 1000000) / 100000);
-            int c4 = 2 * (floor(CCN % 100000000) / 10000000);
-            int c5 = 2 * (floor(CCN % 10000000000) / 1000000000);
-            int c6 = 2 * (floor(CCN % 1000000000000) / 100000000000);
-            int c7 = 2 * (floor(CCN % 100000000000000) / 10000000000000);
-            int c8 = 2 * (floor(CCN % 10000000000000000) / 1000000000000000);
+            int c1 = (floor(CNN % 10) / 1);
+            int c2 = 2 * (floor(CCN % 100) / 10);
+            int c4 = 2 * (floor(CCN % 10000) / 1000);
+            int c6 = 2 * (floor(CCN % 1000000) / 100000);
+            int c8 = 2 * (floor(CCN % 100000000) / 10000000);
+            int c10 = 2 * (floor(CCN % 10000000000) / 1000000000);
+            int c12 = 2 * (floor(CCN % 1000000000000) / 100000000000);
+            int c14 = 2 * (floor(CCN % 100000000000000) / 10000000000000);
+            int c16 = 2 * (floor(CCN % 10000000000000000) / 1000000000000000);
             //printf("Each one is: %i, %i, %i, %i, %i, %i, %i, %i\n", check1, check2, check3, check4, check5, check6, check7, check8);
-            int add = ((c1 / 10) + (c1 % 10) + (c2 / 10) + (c2 % 10) + (c3 / 10) + (c3 % 10) + (c4 / 10) + (c4 % 10) + (c5 / 10) + (c5 % 10) + (c6 / 10) + (c6 % 10) + (c7 / 10) + (c7 % 10) + (c8 / 10) + (c8 % 10))
+            int add = ((c2 / 10) + (c2 % 10) + (c4 / 10) + (c4 % 10) + (c6 / 10) + (c6 % 10) + (c8 / 10) + (c8 % 10) + (c10 / 10) + (c10 % 10) + (c12 / 10) + (c12 % 10) + (c14 / 10) + (c14 % 10) + (c16 / 10) + (c16 % 10))
 
         }
         else if (MASTERCARD16 == 51 || MASTERCARD16 == 52 || MASTERCARD16 == 53 || MASTERCARD16 == 54 || MASTERCARD16 == 55)
