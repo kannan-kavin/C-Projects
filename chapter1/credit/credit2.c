86711a70c1d48e0dc1f4656e571311550cd10dae
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-17 16:40:13 +0000

    Tue, Oct 17, 2023, 12:40 PM -04:00

--- chapter1/credit/credit2.c
+++ chapter1/credit/credit2.c
@@ -7,7 +7,11 @@ int main(void)
     long CCN;
     CCN = get_long("Enter Credit Card Number: ");
     int d = 0;
-    for ()
-    while (CCN < 0);
-    printf("CNN:%ld d:%i", CCN, d);
+    long determinant = CCN;
+    while (determinant > 0)
+    {
+        d = d+1;
+        determinant = (determinant / 10);
+    }
+    printf("determinant:%ld d:%i\n", determinant, d);
 }
