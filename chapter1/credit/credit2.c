b2993d3483cd9e2e4a55a0b88de91a1e1952d6ad
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-18 19:24:32 +0000

    Wed, Oct 18, 2023, 3:24 PM -04:00

--- chapter1/credit/credit2.c
+++ chapter1/credit/credit2.c
@@ -19,12 +19,13 @@ int main(void)
         {
             step2 = (step2 + (CCN % 10));
         }
-        printf("step1sum: %i, step2sum: %i\n", step1, step2);
+        // printf("step1sum: %i, step2sum: %i\n", step1, step2);
     }
     // printf("digits: %i\n", d)
     // step1 keeps track of Step 1 (every other digit added together starting from the second (we'll multiply by 2 later))
     // step2 keeps track of Step 2 (every other digit added together)
     // d keeps track of digits in the card
+    
     if (d == 13)
     {
         printf("VISA\n");
