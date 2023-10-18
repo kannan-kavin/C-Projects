eb4b84b282de0ddd8030d468719837eef6960318
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-18 19:21:42 +0000

    Wed, Oct 18, 2023, 3:21 PM -04:00

--- chapter1/credit/credit2.c
+++ chapter1/credit/credit2.c
@@ -26,7 +26,6 @@ int main(void)
     // step1 keeps track of Step 1 (every other digit added together starting from the second (we'll multiply by 2 later))
     // step2 keeps track of Step 2 (every other digit added together)
     // d keeps track of digits in the card
-
     if (d == 13)
     {
         printf("VISA\n");
