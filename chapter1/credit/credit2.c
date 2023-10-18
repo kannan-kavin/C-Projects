dedcc4f072a79bd398079a26c2540e07e56b68db
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-18 20:34:10 +0000

    Wed, Oct 18, 2023, 4:34 PM -04:00

--- chapter1/credit/credit2.c
+++ chapter1/credit/credit2.c
@@ -47,9 +47,13 @@ int main(void)
         else if (d == 16)
         {
         // printf("VISA OR MASTERCARD\n");
-            if
+            if ((floor(CCN % 10000000000000000) / 1000000000000000) == 4)
             {
-                
+                printf("VISA\n");
+            }
+            else
+            {
+                printf("MASTERCARD\n");
             }
         }
     }
