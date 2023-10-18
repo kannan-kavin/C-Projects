d55d2456a6f8f0361ae328e7b9c62e23b875c2ec
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-18 19:32:16 +0000

    Wed, Oct 18, 2023, 3:32 PM -04:00

--- chapter1/credit/credit2.c
+++ chapter1/credit/credit2.c
@@ -5,27 +5,35 @@ int main(void)
 {
     long CCN;
     CCN = get_long("Enter Credit Card Number: ");
-    int d;
-    int step1 = 0;
-    int step2 = (CCN % 10);
-    for(d = 0; CCN > 0; d++)
+    int step1sum = 0;
+    int step1helper = 0;
+    int step2sum = (CCN % 10);
+    for(int d = 0; CCN > 0; d++)
     {
         CCN = (CCN / 10);
         if (d % 2 == 0)
         {
-            step1 = (step1 + (CCN % 10));
+            step1helper = (step1helper + (CCN % 10));
+            if (step1helper < 10)
+            {
+                step1sum = step1helper +
+            }
+            else (step1helper >= 10)
+            {
+                step1helper = (step1helper / 10) + (step1helper % 10);
+            }
         }
         else
         {
-            step2 = (step2 + (CCN % 10));
+            step2sum = (step2sum + (CCN % 10));
         }
-        // printf("step1sum: %i, step2sum: %i\n", step1, step2);
+        printf("step1sum: %i, step2sum: %i\n", step1sum, step2sum);
     }
     // printf("digits: %i\n", d)
     // step1 keeps track of Step 1 (every other digit added together starting from the second (we'll multiply by 2 later))
     // step2 keeps track of Step 2 (every other digit added together)
     // d keeps track of digits in the card
-    
+
     if (d == 13)
     {
         printf("VISA\n");
