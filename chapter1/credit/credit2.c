bb4ad63dc74974b0e8c2f6d981b2545e43c0ce66
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-18 18:17:59 +0000

    Wed, Oct 18, 2023, 2:17 PM -04:00

--- chapter1/credit/credit2.c
+++ chapter1/credit/credit2.c
@@ -16,9 +16,10 @@ int main(void)
     }
     // printf("determinant:%ld d:%i\n", determinant, d);
     int i;
+    int runningsum = 0;
     for(i = d; i>0; i -= 2)
     {
-        int runningsum = (CCN / pow(10, i));
+        runningsum = runningsum + (CCN / pow(10, i));
         printf("sum: %i\n", runningsum);
     }
 
