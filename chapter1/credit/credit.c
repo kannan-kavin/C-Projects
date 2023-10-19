2e6ba03076033834f8fe1e0536708d6d05bc40cb
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-19 00:50:48 +0000

    Wed, Oct 18, 2023, 8:50 PM -04:00

--- chapter1/credit/credit.c
+++ chapter1/credit/credit.c
@@ -44,7 +44,7 @@ int main(void)
             printf("VISA\n");
         }
         int AMEX2 = (ogCCN / 100000000000000);
-        else if (d == 15 && AMEX2 == 34 || 37)
+        else if (d == 15 && (AMEX2 == 34 || 37))
         {
             printf("AMEX\n");
         }
