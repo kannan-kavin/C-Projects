a288eeb06c066b511479f7b98c66db907127a702
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-25 18:43:57 +0000

    Wed, Oct 25, 2023, 2:43 PM -04:00

---
+++ chapter2/dblletter/dblletter.c
@@ -0,0 +1,28 @@
+/*
+ * dblletter.c
+ * YOUR TASK: Complete the code below so that double letters are removed
+ * Example: An input of Halloween -> Halowen
+ *
+ * Check your work: check50 shoutiss/checks/master/dblletter
+ * Submit your work: submit50 shoutiss/checks/master/dblletter
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Get message from user:
+    string message = get_string("Enter message: ");
+
+    // Print first character
+    printf("%c", message[0]);
+
+    // Iterate through string
+    for (int i = 0, n = strlen(message); i < n - 1; i++)
+    {
+        // YOUR CODE HERE
+    }
+    printf("\n");
+}
