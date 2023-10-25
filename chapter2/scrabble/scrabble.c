be9778d38db3d9d23e1a440a17c5244ee38402f6
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-25 19:39:52 +0000

    Wed, Oct 25, 2023, 3:39 PM -04:00

--- chapter2/scrabble/scrabble.c
+++ chapter2/scrabble/scrabble.c
@@ -21,7 +21,17 @@ int main(void)
     // TODO: Print the winner
     // printf("%i, %i", score1, score2);
     if(score1 > score2)
-    
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
