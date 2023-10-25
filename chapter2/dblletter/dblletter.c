d05ffbf8d15c06842e82f967f678c335d4cd3068
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-25 19:08:52 +0000

    Wed, Oct 25, 2023, 3:08 PM -04:00

--- chapter2/dblletter/dblletter.c
+++ chapter2/dblletter/dblletter.c
@@ -2,7 +2,7 @@
  * dblletter.c
  * YOUR TASK: Complete the code below so that double letters are removed
  * Example: An input of Halloween -> Halowen
- *
+ * done with nishil patel from f block computer science with the esteemed mr. shoutis at the helm of this powerhouse of a class we're all about to code our ways into top universities and change the world.
  * Check your work: check50 shoutiss/checks/master/dblletter
  * Submit your work: submit50 shoutiss/checks/master/dblletter
 */
