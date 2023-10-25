282e95856c816508988d8000dd2a656009bf1d71
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-25 19:27:04 +0000

    Wed, Oct 25, 2023, 3:27 PM -04:00

--- chapter2/scrabble/scrabble.c
+++ chapter2/scrabble/scrabble.c
@@ -27,7 +27,7 @@ int compute_score(string word)
     int score = 0;
     for (int i = 0, n = strlen(word); i < n - 1; i++)
     {
-        word[i] 
+        tolower(word[i]) = 
         score = score + POINTS[]
     }
 }
