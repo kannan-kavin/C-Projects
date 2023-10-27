ee9ed15ca5d0d583afa7c2862cb70adc2566eca0
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-27 18:20:30 +0000

    Fri, Oct 27, 2023, 2:20 PM -04:00

--- chapter2/friends/pennies.c
+++ chapter2/friends/pennies.c
@@ -4,8 +4,8 @@
 
 int main(void)
 {
-    int days = get_int(Days in month: );
-    int pennies = get_int(Pennies on first day: );
+    int days = get_int("Days in month: \n");
+    int pennies = get_int("Pennies on first day: \n");
     pennies = (pennies * pow(2, days);
     float dollars = (pennies / 100.0);
     printf("$%i\n", dollars)
