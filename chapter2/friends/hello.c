ef065c5e7a0b123a99a0735f9b2e02323d9f157e
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-27 17:51:24 +0000

    Fri, Oct 27, 2023, 1:51 PM -04:00

---
+++ chapter2/friends/hello.c
@@ -0,0 +1,17 @@
+/*
+hello girlypop
+i am coding
+greetings earthlings
+*/
+
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./hello <name>\n");
+        return 1;
+    }
+    printf("Hello, world\n");
+}
