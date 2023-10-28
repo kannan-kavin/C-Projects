ebd50dad1f3fa34da9283d4ecd724f2c832647ca
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-28 12:20:04 +0000

    Sat, Oct 28, 2023, 8:20 AM -04:00

--- extra/pennies/pennies.c
+++ extra/pennies/pennies.c
@@ -4,5 +4,11 @@
 
 int main(void)
 {
-    
+    int Pennies
+    Pennies = get_int("Pennies: \n")
+    int days
+    days = get_int("Days: \n")
+    int Dollars
+    Dollars = pow(pennies, days)
+    Dollars = (Dollars / 100)
 }
