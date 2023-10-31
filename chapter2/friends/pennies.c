6ea2f9d2fa2bd494cb89c5684f07942d13e3fda6
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-31 16:46:02 +0000

    Tue, Oct 31, 2023, 12:46 PM -04:00

--- chapter2/friends/pennies.c
+++ chapter2/friends/pennies.c
@@ -12,8 +12,8 @@ int main(int argc, string argv[])
     }
     if ((atoi(argv[1]) >= 28 || atoi(argv[1]) <= 31) && atoi(argv[2]) > 1)
     {
-        int pennies = (atoi(argv[2]) * pow(2, atoi(argv[1])));
-        float dollars = (pennies / 100.0);
+        long long pennies = (atoi(argv[2]) * pow(2, atoi(argv[1])));
+        double dollars = (pennies / 100.0);
         printf("$%.2F\n", dollars);
         return 0;
     }
