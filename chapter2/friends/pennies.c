9b70a8c17c3bbd06c6d87abc28db59f114aa6529
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-10-31 16:38:50 +0000

    Tue, Oct 31, 2023, 12:38 PM -04:00

--- chapter2/friends/pennies.c
+++ chapter2/friends/pennies.c
@@ -7,12 +7,12 @@ int main(int argc, string argv[])
 {
     if (argc != 3)
     {
-        printf("Usage: ./pennies <# of Pennies and # of Days>\n");
+        printf("Usage: ./pennies <# Days and # Pennies>\n");
         return 1;
     }
-    if ((atoi(argv[2]) >= 28 || atoi(argv[2]) <= 31) && atoi(argv[1]) > 1)
+    if ((atoi(argv[1]) >= 28 || atoi(argv[1]) <= 31) && atoi(argv[2]) > 1)
     {
-        int pennies = (atoi(argv[1]) * pow(2, atoi(argv[2])));
+        int pennies = (atoi(argv[2]) * pow(2, atoi(argv[1])));
         float dollars = (pennies / 100.0);
         printf("$%.2F\n", dollars);
     }
