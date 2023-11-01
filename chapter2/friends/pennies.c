4bfc02c8a3d5619b3ccd0fe88f411854ea7c05de
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-01 18:36:56 +0000

    Wed, Nov 1, 2023, 2:36 PM -04:00

--- chapter2/friends/pennies.c
+++ chapter2/friends/pennies.c
@@ -7,7 +7,7 @@ int main(int argc, string argv[])
 {
     if (argc != 3)
     {
-        printf("Usage: ./pennies <# Days and # Pennies>\n");
+        printf("Usage: ./pennies <# Days> <# Pennies>\n");
         return 1;
     }
     if ((atoi(argv[1]) >= 28 || atoi(argv[1]) <= 31) && atoi(argv[2]) > 1)
