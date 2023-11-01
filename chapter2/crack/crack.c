e8f444ab015e47deaad935fe7aa146552087ec5d
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-01 19:08:44 +0000

    Wed, Nov 1, 2023, 3:08 PM -04:00

--- chapter2/crack/crack.c
+++ chapter2/crack/crack.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <stdlib.h>
+#include <crypt.h>
 
 int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./crack <password>")
+        printf("Usage: ./crack hash")
         return 1;
     }
 }
