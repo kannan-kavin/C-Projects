00f1b741898d862a323c479abd2f78eb7399b770
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-01 18:43:23 +0000

    Wed, Nov 1, 2023, 2:43 PM -04:00

--- chapter2/friends/pennies.c
+++ chapter2/friends/pennies.c
@@ -1,21 +1,40 @@
+// Calculates how many pennies a user would get if their change doubles each day using command line information
+
 #include <cs50.h>
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, string argv[])
+// Defined constants
+#define MAX_DAYS 31
+#define MIN_DAYS 28
+
+int main(void)
 {
-    if (argc != 3)
+    // Get a number of days in [28, 31]
+    int days;
+    do
     {
-        printf("Usage: ./pennies <# Days> <# Pennies>\n");
-        return 1;
+        days = get_int("Days in month: ");
     }
-    int days = atoi(argv[1]);
-    if (days < 28 || days > 31)
+    while (days < MIN_DAYS || days > MAX_DAYS);
+
+    // Get a number of pennies in [0, inf)
+    int pennies;
+    do
     {
-        long long pennies = (atoi(argv[2]) * pow(2, days));
-        double dollars = (pennies / 100.0);
-        printf("$%.2f\n", dollars);
-        return 0;
+        pennies = get_int("Pennies on first day: ");
     }
+    while (pennies <= 0);
+
+    // Relatively few pennies required to get out of bounds of int
+    long long total = pennies;
+
+    // Add double the amount of pennies of the previous day each day
+    for (int i = 1; i < days; i++)
+    {
+        total += pennies * pow(2, i);
+    }
+
+    // Output the total as dollars and cents only
+    printf("$%.2f\n", ((double) total) / 100);
 }
