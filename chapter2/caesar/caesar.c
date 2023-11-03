7eec58f0b01ec2786fda5aff3bbafd475333a9c6
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-03 17:49:54 +0000

    Fri, Nov 3, 2023, 1:49 PM -04:00

---
+++ chapter2/caesar/caesar.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    // Get a key from the user
+    if (argc != 2 || atoi(argv[1]) != )
+    {
+        printf("Usage: ./caesar <key>\n");
+        return 1;
+    }
