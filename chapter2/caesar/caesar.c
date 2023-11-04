1c5629460172d9d5879d502217f6655715bdc7bb
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-04 14:45:07 +0000

    Sat, Nov 4, 2023, 10:45 AM -04:00

--- chapter2/caesar/caesar.c
+++ chapter2/caesar/caesar.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-string caesariphy(string plain)
+string caesariphy(string plain);
 
 int main(int argc, string argv[])
 {
@@ -13,18 +13,18 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar <key>\n");
         return 1;
     }
-    string plain = get_string("plaintext: ")
+    string plain = get_string("plaintext: ");
     // printf("%c\n", plain[0]);
-    printf("ciphertext: %s\n", cipher)
+    printf("ciphertext: %s\n", cipher);
 }
 
-string caesariphy(string plain)
+string caesariphy(string plain);
 {
     for (int i = 0, n = strlen(plain); i < n, i++)
     // increment through each letter and convert it using the key (argv[1])
-    char character = (plain[i])
+    char character = (plain[i]);
     if (argv[1] + character > 122)
-    
+
     if (character <= 122 || character >= 97)
     {
 
