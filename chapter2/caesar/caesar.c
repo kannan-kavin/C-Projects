539bbcc31b55f79fb0f23b1d37b4733da65a7f7a
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-08 20:29:33 +0000

    Wed, Nov 8, 2023, 3:29 PM -05:00

--- chapter2/caesar/caesar.c
+++ chapter2/caesar/caesar.c
@@ -15,14 +15,15 @@ int main(int argc, string argv[])
     string plain = get_string("plaintext: ");
     // printf("%c\n", plain[0]);
     int key = (atoi(argv[1]) % 26);
-    for (int i = 0, n = strlen(plain); i < n, i++)
+    int n = strlen(plain);
+    for (int i = 0; i < n; i++)
     // increment through each letter and convert it using the key (argv[1])
     char character = (plain[i]);
     if (character <= 122 && character >= 97)
     {
         cipher[i] = 96 + ((character - 96 + key) % 26);
     }
-    if (character >= 65 && character <=90 )
+    if (character >= 65 && character <=90)
     {
         cipher[i] = 65 + ((character - 65 + key) % 26);
     }
