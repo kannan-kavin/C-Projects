ab6b27c33404ca546c3c606743700b5d54e0a529
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-08 20:20:42 +0000

    Wed, Nov 8, 2023, 3:20 PM -05:00

--- chapter2/caesar/caesar.c
+++ chapter2/caesar/caesar.c
@@ -19,7 +19,7 @@ int main(int argc, string argv[])
     char character = (plain[i]);
     if (character <= 122 && character >= 97)
     {
-        97 + (character - 97 + key)
+        97 + ((character - 96 + key) % 26)
     }
     if (character >= 65 && character <=90 )
     {
