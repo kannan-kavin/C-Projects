dab97b16f5fc68c10ae02f76fb2579755d7d8a17
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-19 09:06:53 +0000

    Sun, Nov 19, 2023, 4:06 AM -05:00

--- chapter2/substitution/substitution.c
+++ chapter2/substitution/substitution.c
@@ -18,7 +18,7 @@ int main(int argc, string argv[])
         if (65 > (keychecker[i]) || (97 > (keychecker[i]) && (keychecker[i]) < 90) || 122 < (keychecker[i]) || strlen(keychecker) != 26)
         {
             printf("Key must be contain 26 characters\n");
-            printf("%i\n", keychecker[i]);
+            // printf("%i\n", keychecker[i]);
             return 1;
         // FIGURE OUT HOW TO LOWKEY DO THIS PLEASE BECAUSE LETTERS CANT BE SAME
         else (keychecker != )
