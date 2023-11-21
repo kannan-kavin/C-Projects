a2b8786c6a77d6a6d2f2e446a6e7ec02b3f19264
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-21 16:47:42 +0000

    Tue, Nov 21, 2023, 11:47 AM -05:00

--- chapter2/substitution/substitution.c
+++ chapter2/substitution/substitution.c
@@ -38,12 +38,12 @@ int main(int argc, string argv[])
         int key = (atoi(argv[1]) % 26);
         if (c <= 122 && c >= 97)
         {
-            text[i] = 97 + ((c - 97 + key) % 26);
+            text[i] = keychecker[i];
         }
         // Shifting all Lowercase Letters
         if (c <= 90 && c >= 65)
         {
-            text[i] = 65 + ((c - 65 + key) % 26);
+            text[i] = keychecker[i];
         }
         // Shifting all Uppercase Letters
     }
