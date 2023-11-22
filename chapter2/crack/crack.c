6f54098154552a8bc3ed22d8623adef9ea2aea9a
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-22 17:26:23 +0000

    Wed, Nov 22, 2023, 12:26 PM -05:00

--- chapter2/crack/crack.c
+++ chapter2/crack/crack.c
@@ -10,4 +10,24 @@ int main(int argc, string argv[])
         printf("Usage: ./crack <hash>\n")
         return 1;
     }
+    string key = (argv[1]);
+    for (int i = 0; i < 8; i++)
+    {
+        if ((strlen(key) != 13))
+        {
+            printf("Key must only contain 13 characters.\n");
+            // DEBUGGING: printf("%i\n", keychecker[i]);
+            return 1;
+        }
+        // Checks for a 26 Letter key 👌
+        for (int j = i + 1; j < 26; j++)
+        {
+            if (key[i] == key[j])
+            {
+                printf("Key must not contain repeated characters.\n");
+                return 1;
+            }
+        }
+        // Checks for independence between letters 👌
+    }
 }
