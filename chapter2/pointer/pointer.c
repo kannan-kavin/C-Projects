d39b83ce84baca1bb6ea14a1e0b920f2b44201a0
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-29 20:07:23 +0000

    Wed, Nov 29, 2023, 3:07 PM -05:00

---
+++ chapter2/pointer/pointer.c
@@ -0,0 +1,10 @@
+// collect 2 int from user, call a function that swaps those values, print the swap (swapping must take place in a function)
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    
+}
