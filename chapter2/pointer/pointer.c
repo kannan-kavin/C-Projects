8554acbd04f4a1057a17910753e849b4696c8de0
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-11-29 20:16:20 +0000

    Wed, Nov 29, 2023, 3:16 PM -05:00

--- chapter2/pointer/pointer.c
+++ chapter2/pointer/pointer.c
@@ -4,10 +4,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, string argv[])
-{
-    if (argc != 3)
-    {
 
-    }
+int main(void)
+{
+    int num1 = get_int("Integer 1: ");
+    int num2 = get_int("Integer 2: ");
+
+    printf("num1: %inum2: %i\n", num1, num2);
+
+    printf("Swapping!\n");
+    swap(num1, num2);
+
+    printf("num1: %inum2: %i\n", num1, num2);
+}
+
+void swap(int num1, int num2)
+{
+    int *ptr = &num1;
+    num1 = num2;
+    num1 = *ptr;
 }
