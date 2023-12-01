1a4bf661baee86f9fb5602fb111e3f54b0783cf5
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-12-01 19:10:56 +0000

    Fri, Dec 1, 2023, 2:10 PM -05:00

---
+++ chapter3/islandmapping/island.c
@@ -0,0 +1,19 @@
+/*Complete the function below
+* This file will not have a main function. DO NOT ADD ONE.
+* Type the next 2 commands in your terminal:
+* make islandmain
+* ./islanmain
+* You can use different test islands to see if your function works.
+* To use different test islands, give input -1, -2, -3, -4
+*/
+
+
+#include <cs50.h>
+#include <stdio.h>
+
+#include "island.h"
+
+int mapping(int island[5][5])
+{
+  
+}
