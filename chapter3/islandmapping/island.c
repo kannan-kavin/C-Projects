19878f1a9a9716cafc0b70c1f83ece0fde461f51
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2023-12-08 19:12:20 +0000

    Fri, Dec 8, 2023, 2:12 PM -05:00

--- chapter3/islandmapping/island.c
+++ chapter3/islandmapping/island.c
@@ -23,19 +23,19 @@ int mapping(int island[5][5])
       {
          if (island[x][y] == 1)
          {
-            if (x == 0)
-            {
-               if ()
-            }
-            if (x == 4)
+            if (island[x-1][y] == 1)
             {
 
             }
-            if (y == 0)
+            if (island[x+1][y] == 1)
             {
 
             }
-            if (y == 4)
+            if (island[x][y+1] == 1)
+            {
+
+            }
+            if (island[x][y-1] == 1)
             {
 
             }
