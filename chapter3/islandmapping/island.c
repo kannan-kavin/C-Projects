a842eddab12b7ae0b01fceb0f309dcf12233f2fd
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-01-04 02:44:34 +0000

    Wed, Jan 3, 2024, 9:44 PM -05:00

--- chapter3/islandmapping/island.c
+++ chapter3/islandmapping/island.c
@@ -66,6 +66,7 @@ int mapping(int island[5][5])
                perimeter = perimeter + 1;
             }
             // Checking all squares above the one just checked
+            printf("perimeter: %i", perimeter);
          }
       }
    }
