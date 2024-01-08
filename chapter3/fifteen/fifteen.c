5dae188a489e6fc82e870ac6f7577c6baf209917
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-01-08 19:10:02 +0000

    Mon, Jan 8, 2024, 2:10 PM -05:00

--- chapter3/fifteen/fifteen.c
+++ chapter3/fifteen/fifteen.c
@@ -227,7 +227,7 @@ bool move(int tile)
 // Returns true if game is won (i.e., board is in winning configuration), else false
 bool won(void)
 {
-    /* int value = (d * d);
+    int value = (d * d);
     for (int i = 0; i < d; i++)
     {
         for (int j = 0; j < d; j++)
@@ -239,5 +239,5 @@ bool won(void)
             }
         }
     }
-    */return false;
+    return true;
 }
