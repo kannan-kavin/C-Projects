9e321cdefc0fecad39ca1b3aa4b0915d0d9fd25f
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-01-09 19:16:34 +0000

    Tue, Jan 9, 2024, 2:16 PM -05:00

--- chapter3/fifteen/fifteen.c
+++ chapter3/fifteen/fifteen.c
@@ -244,6 +244,13 @@ bool won(void)
     {
         for (int j = 0; j < (d - 1); j++)
         {
+            if (board[i][j] == board[d - 1][d - 1])
+            {
+                if (board[i][j] != 0)
+                {
+                    return false;
+                }
+            }
             if (board[i][j] != value)
             {
                 return false;
@@ -251,6 +258,6 @@ bool won(void)
             value = value + 1;
         }
     }
-    
     return false;
 }
+// 7 and 8
