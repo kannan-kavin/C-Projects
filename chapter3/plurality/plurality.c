1091fdf81a953b48e8bb76138ae26d84a72fa52e
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-01-17 20:17:00 +0000

    Wed, Jan 17, 2024, 3:17 PM -05:00

--- chapter3/plurality/plurality.c
+++ chapter3/plurality/plurality.c
@@ -79,6 +79,20 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    
+    int maxvotes = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > maxvotes)
+        {
+            candidates[i].votes = maxvotes;
+        }
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == maxvotes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
     return;
 }
