f903dcb3104fba2da3ebce589a2a17ac8bc32271
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-01-17 20:25:08 +0000

    Wed, Jan 17, 2024, 3:25 PM -05:00

--- chapter3/plurality/plurality.c
+++ chapter3/plurality/plurality.c
@@ -69,7 +69,7 @@ bool vote(string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            candidates[i].votes++;
+            candidates[i].votes = candidates[i].votes + 1;
             return true;
         }
     }
