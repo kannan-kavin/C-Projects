17df0586504686678cea01cf33f2ddbe1e5a2b64
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-02-07 20:29:40 +0000

    Wed, Feb 7, 2024, 3:29 PM -05:00

--- chapter3/runoff/runoff/runoff.c
+++ chapter3/runoff/runoff/runoff.c
@@ -129,10 +129,10 @@ bool vote(int voter, int rank, string name)
     for (int i = 0; i < voter_count; i++)
         for (int j = 0; j < candidate_count; j++)
             {
-                if (strcmp(candidates.name, name) == 0)
+                if (strcmp(candidates[j].name, name) == 0)
                 {
-                    candidates[i].votes = candidates[i].votes + 1;
-                    preferences[i][j] =
+                    candidates[j].votes++;
+                    preferences[i][j] = 
                     return true;
                 }
             }
