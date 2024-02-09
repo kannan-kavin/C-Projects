8ccc84da653f053562daa5d42a62e952b5b57b13
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-02-09 19:23:23 +0000

    Fri, Feb 9, 2024, 2:23 PM -05:00

--- chapter3/runoff/runoff/runoff.c
+++ chapter3/runoff/runoff/runoff.c
@@ -141,14 +141,14 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    
+
     return;
 }
 
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    
     return false;
 }
 
