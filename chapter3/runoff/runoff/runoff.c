8861413227b99549b203761d657d6487b1f68c9a
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-02-09 18:54:15 +0000

    Fri, Feb 9, 2024, 1:54 PM -05:00

--- chapter3/runoff/runoff/runoff.c
+++ chapter3/runoff/runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
             if (strcmp(candidates[].name, name) == 0)
             {
                 candidates[j].votes++;
-                preferences[i][j] = 
+                preferences[i][j] =
                 return true;
             }
         }
@@ -141,7 +141,7 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    
     return;
 }
 
