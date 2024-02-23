84de607f3127bece4150a2f8071dead7ac17b74e
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-02-23 18:25:53 +0000

    Fri, Feb 23, 2024, 1:25 PM -05:00

--- chapter3/runoff/runoff/runoff.c
+++ chapter3/runoff/runoff/runoff.c
@@ -126,12 +126,14 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
+    int prefer = 0;
     for (int i = 0; i < voter_count; i++)
         {
             if (strcmp(candidates[i].name, name) == 0)
             {
                 candidates[i].votes++;
                 preferences[i][j] =
+                prefer++;
                 return true;
             }
         }
