54c938eb0532faa909e30d9b49c77529bba0ac8d
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-02-23 19:19:20 +0000

    Fri, Feb 23, 2024, 2:19 PM -05:00

--- chapter3/runoff/runoff.c
+++ chapter3/runoff/runoff.c
@@ -141,7 +141,10 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp )
+    }
     return;
 }
 
