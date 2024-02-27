39b096263e42a454335e73a2d56339fc946ed058
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-02-27 17:46:35 +0000

    Tue, Feb 27, 2024, 12:46 PM -05:00

--- chapter3/runoff/runoff.c
+++ chapter3/runoff/runoff.c
@@ -147,7 +147,7 @@ void tabulate(void)
         {
             for (int k = 0; k < 3; k++)
             {
-                if (strcmp(preferences[i][k] = candidates[j]) && candidates[j].eliminated == 0)
+                if (strcmp(preferences[i][k], candidates[j]) == 0 && candidates[j].eliminated == 0)
                 {
                     candidates[j].votes +=;
                 }
