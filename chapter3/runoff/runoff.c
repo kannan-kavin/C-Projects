d0d1e02094432584088115f5ad923184d6f33b98
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-02-28 20:23:04 +0000

    Wed, Feb 28, 2024, 3:23 PM -05:00

--- chapter3/runoff/runoff.c
+++ chapter3/runoff/runoff.c
@@ -150,7 +150,7 @@ void tabulate(void)
                 if (preferences[i][k] == j && candidates[j].eliminated == 0)
                 {
                     candidates[j].votes++;
-                    
+                    if
                 }
             }
         }
