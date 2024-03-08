7cbee9bdb46f153f076da194fcd0199b7490fdc0
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-08 18:31:43 +0000

    Fri, Mar 8, 2024, 1:31 PM -05:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -3,7 +3,12 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < width; i++)
+    {}
+        for (int j = 0; j < height, j++)
+        {
+
+        }
     return;
 }
 
