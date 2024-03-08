6690b127b29d9c10c025bb9f176d743cf0a82d6f
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-08 18:44:27 +0000

    Fri, Mar 8, 2024, 1:44 PM -05:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < height; j++)
         {
-            float average = round((image[j][i].rgbtRed + image[j][i].rgbtGreen + image[j][i].rgbtBlue) / 3);
+            float average = ((image[j][i].rgbtRed + image[j][i].rgbtGreen + image[j][i].rgbtBlue) / 3);
             image[j][i].rgbtRed = average;
             image[j][i].rgbtGreen = average;
             image[j][i].rgbtBlue = average;
