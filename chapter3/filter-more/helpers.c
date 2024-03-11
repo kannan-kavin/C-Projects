804ba998a3f0ca53d95883e4a20443e61475dd2e
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-11 18:06:13 +0000

    Mon, Mar 11, 2024, 2:06 PM -04:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -93,12 +93,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    /* for (int i = 0; i < width; i++)
+    for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
         {
 
         }
     }
-    return; */
+    return;
 }
