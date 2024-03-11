35bc667ed5928b0ba54fc2c7356ab32ffe798177
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-11 17:44:17 +0000

    Mon, Mar 11, 2024, 1:44 PM -04:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -7,14 +7,18 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
+        // Iterating through the image pixel array
         {
             float red = (float)image[j][i].rgbtRed;
             float green = (float)image[j][i].rgbtGreen;
             float blue = (float)image[j][i].rgbtBlue;
+            // Turning each RGB value into a float
             float average = round((red + green + blue) / 3);
+            // Averaging the colors
             image[j][i].rgbtRed = average;
             image[j][i].rgbtGreen = average;
             image[j][i].rgbtBlue = average;
+            // Assiging the Average back to the pixel to "Gray" the image
         }
     }
     return;
@@ -26,10 +30,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < (width / 2); i++)
     {
         for (int j = 0; j < height; j++)
+        // Iterating through the image pixel array stopping halfway through)
         {
             RGBTRIPLE temp = image[j][i];
+            // Making a temporary array keeping the normal pixel
             image[j][i] = image[j][width-1-i];
+            // Assigning the pixel its reflected pixel
             image[j][width-1-i] = temp;
+            // Assigning the reflected pixel its pixel using the temporary array
         }
     }
     return;
@@ -39,18 +47,21 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE average[height][width];
-
+    // Creating a new array that contains the averages of all the colors around each pixel based on the image array
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
+        // Iterating through the image pixel array
         {
             float red = 0;
             float green = 0;
             float blue = 0;
+            // Creating values for each pixel that will be used to find the average
             float denominator = 0;
             for (int k = -1; k < 2; k++)
             {
                 for (int l = -1; l < 2; l++)
+                // Iterating around the pixel we are using
                 {
                     if (i + k >= 0 && i + k < width && j + l >= 0 && j + l < height)
                     {
