7e72e06b7b6b9681e441cd645b59f10078f6a41d
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-12 16:34:16 +0000

    Tue, Mar 12, 2024, 12:34 PM -04:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -136,7 +136,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                                 runningtotaly += value;
                             }
                         }
-                        matrix[j][i].rgbtRed = (sqrt(runningtotalx) + sqrt(runningtotaly));
+                        matrix[j][i].rgbtRed = (sqrt(runningtotalx + runningtotaly));
                         // RED
                         runningtotalx = 0;
                         runningtotaly = 0;
@@ -157,7 +157,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                                 runningtotaly += value;
                             }
                         }
-                        matrix[j][i].rgbtGreen = (sqrt(runningtotalx) + sqrt(runningtotaly));
+                        matrix[j][i].rgbtGreen = (sqrt(runningtotalx + runningtotaly));
                         // GREEN
                         runningtotalx = 0;
                         runningtotaly = 0;
@@ -178,7 +178,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                                 runningtotaly += value;
                             }
                         }
-                        matrix[j][i].rgbtBlue = (sqrt(runningtotalx) + sqrt(runningtotaly));
+                        matrix[j][i].rgbtBlue = (sqrt(runningtotalx + runningtotaly));
                         // BLUE
                     }
                 }
