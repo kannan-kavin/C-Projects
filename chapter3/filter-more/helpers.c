e6e1d618d313a252a891337f920a37e9826a4689
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-12 15:57:58 +0000

    Tue, Mar 12, 2024, 11:57 AM -04:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -113,7 +113,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 for (int l = -1; l < 2; l++)
                 // Iterating around the pixel we are using as our center
                 {
-                    
+                    if ()
                 }
             }
         }
