c572c094083603329f07cba6d8394a60ddcae7de
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-12 16:41:08 +0000

    Tue, Mar 12, 2024, 12:41 PM -04:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -107,9 +107,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < height; j++)
         // Iterating through the image pixel array
-        int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
         int runningtotalx = 0;
-        int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
         int runningtotaly = 0;
         {
             for (int k = -1; k < 2; k++)
@@ -117,6 +115,8 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 for (int l = -1; l < 2; l++)
                 // Iterating around the pixel we are using as our center
                 {
+                    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+                    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
                     if (i + k >= 0 && i + k < width && j + l >= 0 && j + l < height)
                     {
                         // Initializng the Sobel Operators (x and y)
