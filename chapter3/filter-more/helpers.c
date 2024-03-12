4784831285fe6312cac5a3cc5968cdfe723bc260
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-12 16:16:14 +0000

    Tue, Mar 12, 2024, 12:16 PM -04:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -116,8 +116,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                     if (i + k >= 0 && i + k < width && j + l >= 0 && j + l < height)
                     {
                         int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-                        int Gy[3][3] = {}
-
+                        int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+                        // Initializng the Sobel Operators (x and y)
+                        
                     }
                 }
             }
