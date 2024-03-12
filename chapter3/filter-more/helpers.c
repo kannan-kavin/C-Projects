2f6820cb86adef8d0c1f36884f813a83de1b7f87
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-12 15:50:29 +0000

    Tue, Mar 12, 2024, 11:50 AM -04:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -103,6 +103,5 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             // Making a copy of the original array to mess around with
         }
     }
-    for (int )
     return;
 }
