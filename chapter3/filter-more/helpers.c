826bd6c889e54fc9db1d83dd6839f5a65cbd59c3
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-12 18:22:03 +0000

    Tue, Mar 12, 2024, 2:22 PM -04:00

--- chapter3/filter-more/helpers.c
+++ chapter3/filter-more/helpers.c
@@ -138,9 +138,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 {
                 if (i + k >= 0 && i + k < width && j + l >= 0 && j + l < height)
                     {
-                        int valuex = (image[j + l][i + k].rgbtGreen * Gx[l][k]);
+                        int valuex = (image[j + l][i + k].rgbtGreen * Gx[l + 1][k + 1]);
                         runningtotalx += valuex;
-                        int valuey = (image[j + l][i + k].rgbtGreen * Gy[l][k]);
+                        int valuey = (image[j + l][i + k].rgbtGreen * Gy[l + 1][k + 1]);
                         runningtotaly += valuey;
                     }
                 }
@@ -156,9 +156,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 {
                     if (i + k >= 0 && i + k < width && j + l >= 0 && j + l < height)
                     {
-                        int valuex = (image[j + l][i + k].rgbtRed * Gx[l][k]);
+                        int valuex = (image[j + l][i + k].rgbtRed * Gx[l + 1][k + 1]);
                         runningtotalx += valuex;
-                        int valuey = (image[j + l][i + k].rgbtRed * Gy[l][k]);
+                        int valuey = (image[j + l][i + k].rgbtRed * Gy[l + 1][k + 1]);
                         runningtotaly += valuey;
                     }
                 }
