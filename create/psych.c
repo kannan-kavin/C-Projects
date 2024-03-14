f1e30a998646f7c0a187dc01f6570d934596ee33
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-14 16:50:39 +0000

    Thu, Mar 14, 2024, 12:50 PM -04:00

---
+++ create/psych.c
@@ -0,0 +1,44 @@
+// When displayed a term, pick the correct definition (a-d)
+
+// Separate into units 1-9
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    // Ensure proper usage
+    if (argc != 2)
+    {
+        printf("Usage: ./psych [unit]\n");
+        return 1;
+    }
+
+    // Ensure valid unit
+    int unit = atoi(argv[1]);
+    if (unit < 1 || unit > 9)
+    {
+        printf("Unit must be between 1 to 9");
+        return 2;
+    }
+    // Prompt the user with the question
+    question();
+    //Get the user's answer
+    char choice = tolower(get_char("What is your answer?\n"));
+    // Congratulate the user if they answer correctly or tell the user the correct answer if they answer incorrectly
+    answer();
+}
+
+// Pick a term to use as the question
+void question(int unit)
+{
+    printf("What does %s mean",)
+}
+
+//
+void answer(char choice)
+{
+
+}
