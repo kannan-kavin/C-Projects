4a4e6b12c123b262b32be73835539d1ba969b66b
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-03-26 18:17:57 +0000

    Tue, Mar 26, 2024, 2:17 PM -04:00

--- create/psych.c
+++ create/psych.c
@@ -145,14 +145,6 @@ void question(int unit)
         }
         while (index1 != indexterm && index2 != indexterm && index3 != indexterm && index1 != index2 && index1 != index3 && index2 != index3);
         string shuffled[4];
-        do
-        {
-            shuffled[rand() % 4] = terms[indexterm].definition;
-            shuffled[rand() % 4] = terms[index1].definition;
-            shuffled[rand() % 4] = terms[index2].definition;
-            shuffled[rand() % 4] = terms[index3].definition;
-        }
-        while (shuffled[0] != shuffled[1] && shuffled[0] != shuffled[2] && shuffled[0] != shuffled[3] && shuffled[1] != shuffled[2] && shuffled[1] != shuffled[3] && shuffled[2] != shuffled[3]);
         printf("What does %s mean?\na.) %s\nb.) %s\nc.) %s\nd.) %s\n", terms[indexterm].term, shuffled[0], shuffled[1], shuffled[2], shuffled[3]);
     }
     if (unit == 2)
