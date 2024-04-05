bfaf4ba0c693f6717f35bf788abbda3de15554df
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-04-05 16:26:12 +0000

    Fri, Apr 5, 2024, 12:26 PM -04:00

--- create/create.c
+++ create/create.c
@@ -1024,6 +1024,7 @@ int main(int argc, string argv[])
     }
     // Prompt the user with the question
     question(unit);
+    string correctanswer = question();
     //Get the user's answer
     string choice = (get_string("What is your answer? (copy and paste it)\n"));
     // Congratulate the user if they answer correctly or tell the user the correct answer if they answer incorrectly
