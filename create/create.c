ec38bdd6d7f58328caf842ce48141c4d6d47f68c
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-04-05 16:12:20 +0000

    Fri, Apr 5, 2024, 12:12 PM -04:00

--- create/create.c
+++ create/create.c
@@ -983,10 +983,10 @@ void question(int unit)
 }
 
 // Using their choice, determine whether the user is correct or not and allow them to try again (FOR LOOP) if they get it wrong
-void answer(string choice, string correctanswer)
+/*void answer(string choice, string correctanswer)
 {
     strcmp()
-}
+}*/
 
 int main(int argc, string argv[])
 {
@@ -1009,5 +1009,5 @@ int main(int argc, string argv[])
     //Get the user's answer
     string choice = (get_string("What is your answer? (copy and paste it)\n"));
     // Congratulate the user if they answer correctly or tell the user the correct answer if they answer incorrectly
-    answer(choice);
+    // answer(choice);
 }
