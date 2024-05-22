3b4b3d61136fba807edf2171bf76dead1dcf03da
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-22 19:25:54 +0000

    Wed, May 22, 2024, 3:25 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[])
         // Checking whether it's the start of a new jpeg
         if(buffer[j] == 0xff && buffer[j + 1] == 0xd8 && buffer[j + 2] == 0xff && (buffer[j + 3] & 0xf0 == 0xe0))
         {
+            sprintf(filename, "%03i.jpg", file_counter);
             // Checking whether it's the first jpeg
             if(Firstjpeg == false)
             {
@@ -32,6 +33,7 @@ int main(int argc, char *argv[])
             {
                 FILE *file = fopen("%03i.jpg", "w");
             }
+            file_counter++;
         }
         // Checking whether it's not the start of a new jpeg
         else
