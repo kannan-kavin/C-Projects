814867536de6ebe792dafe394fc00e534d421a23
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-22 19:02:16 +0000

    Wed, May 22, 2024, 3:02 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
@@ -36,5 +37,6 @@ int main(int argc, char *argv[])
         {
             j++
         }
+        j++
     }
 }
