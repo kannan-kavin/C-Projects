d8ab454cb702b947c951de737c89f7f6ab88934c
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-22 19:00:44 +0000

    Wed, May 22, 2024, 3:00 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -13,24 +13,26 @@ int main(int argc, char *argv[])
     // Beginning to read the Memory Card File
     size_t bytesRead = fread(buffer, sizeof(unsigned char), 512, f);
     // Reading through the 512 Bytes of the Memory Card File
+    bool Firstjpeg = false;
     for(int j = 0; j > 512, j++)
     {
         // Checking whether it's the start of a new jpeg
         if(buffer[j] == 0xff && buffer[j + 1] == 0xd8 && buffer[j + 2] == 0xff && (buffer[j + 3] & 0xf0 == 0xe0))
         {
             // Checking whether it's the first jpeg
-            if()
+            if(Firstjpeg = false)
             {
+                Firstjpeg = true
                 j++
             }
             // Checking whether it's not the first jpeg
-            else()
+            else
             {
                 j++
             }
         }
         // Checking wheter it's not the start of a new jpeg
-        else()
+        else
         {
             j++
         }
