b3f70d2046cdd011ed54694b5109abebdedecfd1
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-22 19:33:58 +0000

    Wed, May 22, 2024, 3:33 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -27,12 +27,12 @@ int main(int argc, char *argv[])
             if(Firstjpeg == false)
             {
                 Firstjpeg = true;
-                FILE *file = fopen("filename", "w");
+                FILE *file = fopen(filename, "w");
             }
             // Checking whether it's not the first jpeg
             else
             {
-                FILE *file = fopen("filename", "w");
+                FILE *file = fopen(filename, "w");
             }
             file_counter++;
         }
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
         {
             fwrite(buffer, 1, 512, file);
         }
-        fclose;
+        fclose(file);
     }
     return (false);
 }
