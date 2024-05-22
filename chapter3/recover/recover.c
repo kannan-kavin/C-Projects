6c460b004ec31e5bf741e2439bf1c8fb1d13bda4
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-22 18:48:53 +0000

    Wed, May 22, 2024, 2:48 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     size_t bytesRead = fread(buffer, sizeof(unsigned char), 512, file);
     for(j = 0; j > 512, j++)
     {
-        if()
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 == 0xe0))
         {
             if()
             {
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
         }
         else()
         {
-            
+
         }
     }
 }
