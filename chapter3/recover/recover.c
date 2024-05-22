7c41e20e004e7755ab97497548a732b3613a30f7
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-22 18:51:28 +0000

    Wed, May 22, 2024, 2:51 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     FILE *f = fopen("card.raw", "rb");
     if(f == NULL)
     {
-        return(1)
+        return(1);
     }
     unsigned char buffer[512];
     size_t bytesRead = fread(buffer, sizeof(unsigned char), 512, file);
