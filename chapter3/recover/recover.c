eaf96507ae9523a7195f3c2841021b574f4179e2
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-24 17:35:58 +0000

    Fri, May 24, 2024, 1:35 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -5,8 +5,12 @@
 int main(int argc, char *argv[])
 {
     // Opening the Memory Card File
+    if(argc != 1)
+    {
+        return(1);
+    }
     FILE *f = fopen("card.raw", "rb");
-    if(f == NULL || argc != 1)
+    if(f == NULL)
     {
         return(1);
     }
