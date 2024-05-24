b2eb50f7c3f755b05e9c49c8fc68d92b6c570d25
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-24 18:21:11 +0000

    Fri, May 24, 2024, 2:21 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -1,22 +1,22 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
     // Checking Command Line
-    if (argc != 2)
+    if(argc != 2)
     {
         return 1;
     }
     // Checking if the File can be Opened
     FILE *f = fopen("card.raw", "rb");
-    if (f == NULL)
+    if(f == NULL)
     {
         printf("Could not open file\n");
         return 1;
     }
-    // Beginning to read the Memory Card File
+     // Beginning to read the Memory Card File
     unsigned char buffer[512];
     bool Firstjpeg = false;
     int file_counter = 0;
@@ -24,16 +24,14 @@ int main(int argc, char *argv[])
     FILE *file = NULL;
     // Reading through the 512 Bytes of the Memory Card File
     size_t bytesRead = fread(buffer, sizeof(unsigned char), 512, f);
-
-    for (int j = 0; j < 512; j++)
+    for(int j = 0; j < 512; j++)
     {
         sprintf(namefile, "%03i.jpg", file_counter);
         // Checking whether it's the start of a new jpeg
-        if (buffer[j] == 0xff && buffer[j + 1] == 0xd8 && buffer[j + 2] == 0xff &&
-            (buffer[j + 3] & 0xf0) == 0xe0)
+        if(buffer[j] == 0xff && buffer[j + 1] == 0xd8 && buffer[j + 2] == 0xff && (buffer[j + 3] & 0xf0) == 0xe0)
         {
             // Checking whether it's the first jpeg
-            if (Firstjpeg == false)
+            if(Firstjpeg == false)
             {
                 Firstjpeg = true;
                 file = fopen(namefile, "wb");
@@ -52,11 +50,11 @@ int main(int argc, char *argv[])
         // Checking whether it's not the start of a new jpeg
         else
         {
-            if (file == NULL)
+            if(file == NULL)
             {
                 return (false);
             }
         }
-        return (false);
     }
+    return (false);
 }
