dddad012a443c44ca313d31a71215afcc17c31f8
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-24 18:17:23 +0000

    Fri, May 24, 2024, 2:17 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -23,40 +23,40 @@ int main(int argc, char *argv[])
     char namefile[8];
     FILE *file = NULL;
     // Reading through the 512 Bytes of the Memory Card File
-        size_t bytesRead = fread(buffer, sizeof(unsigned char), 512, f);
+    size_t bytesRead = fread(buffer, sizeof(unsigned char), 512, f);
 
-        for (int j = 0; j < 512; j++)
+    for (int j = 0; j < 512; j++)
+    {
+        sprintf(namefile, "%03i.jpg", file_counter);
+        // Checking whether it's the start of a new jpeg
+        if (buffer[j] == 0xff && buffer[j + 1] == 0xd8 && buffer[j + 2] == 0xff &&
+            (buffer[j + 3] & 0xf0) == 0xe0)
         {
-            sprintf(namefile, "%03i.jpg", file_counter);
-            // Checking whether it's the start of a new jpeg
-            if (buffer[j] == 0xff && buffer[j + 1] == 0xd8 && buffer[j + 2] == 0xff &&
-                (buffer[j + 3] & 0xf0) == 0xe0)
+            // Checking whether it's the first jpeg
+            if (Firstjpeg == false)
             {
-                // Checking whether it's the first jpeg
-                if (Firstjpeg == false)
-                {
-                    Firstjpeg = true;
-                    file = fopen(namefile, "wb");
-                    fwrite(buffer, 1, 512, file);
-                    file_counter++;
-                }
-                // Checking whether it's not the first jpeg
-                else
-                {
-                    fclose(file);
-                    file = fopen(namefile, "wb");
-                    fwrite(buffer, 1, 512, file);
-                    file_counter++;
-                }
+                Firstjpeg = true;
+                file = fopen(namefile, "wb");
+                fwrite(buffer, 1, 512, file);
+                file_counter++;
             }
-            // Checking whether it's not the start of a new jpeg
+            // Checking whether it's not the first jpeg
             else
             {
-                if (file == NULL)
-                {
-                    return (false);
-                }
+                fclose(file);
+                file = fopen(namefile, "wb");
+                fwrite(buffer, 1, 512, file);
+                file_counter++;
             }
+        }
+        // Checking whether it's not the start of a new jpeg
+        else
+        {
+            if (file == NULL)
+            {
+                return (false);
+            }
+        }
         return (false);
     }
 }
