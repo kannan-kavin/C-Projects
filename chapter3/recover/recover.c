34ff61d8577f18905f5e2e107a77f76b533562c0
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-26 22:29:06 +0000

    Sun, May 26, 2024, 6:29 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
         }
         // Creating a file for a new jpeg writing in binary
         sprintf(namefile, "%03i.jpg", file_counter);
-        file = fopen(filename, wb)
+        file = fopen(namefile, "wb");
         if (first_jpeg == true)
         {
             fwrite(buffer, sizeof(unsigned char), 512, file);
