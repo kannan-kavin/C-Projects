a54955acb8fe9e6f9ae4dd0721fca6dac22e169c
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-26 22:15:57 +0000

    Sun, May 26, 2024, 6:15 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
     char namefile[8];
     FILE *file = NULL;
     // Reading through the 512 Bytes of the Memory Card File
-    size_t bytesRead = fread(buffer, sizeof(unsigned char), 512, f);
+    fread(buffer, sizeof(unsigned char), 512, f);
         sprintf(namefile, "%03i.jpg", file_counter);
         // Checking whether it's the start of a new jpeg
         if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
