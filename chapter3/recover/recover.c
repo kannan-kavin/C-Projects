9932bf9db65a8841817bef88096fae598e636e64
kannan-kavin <144480687+kannan-kavin@users.noreply.github.com>
2024-05-26 22:21:48 +0000

    Sun, May 26, 2024, 6:21 PM -04:00

--- chapter3/recover/recover.c
+++ chapter3/recover/recover.c
@@ -1,18 +1,18 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
     // Checking Command Line
-    if(argc != 2)
+    if (argc != 2)
     {
-        printf("Usage ./recover <raw data>\n")
+        printf("Usage ./recover <raw data>\n");
         return 1;
     }
     // Checking if the Binary File provided by the user can be Opened
     FILE *f = fopen(argv[1], "rb");
-    if(f == NULL)
+    if (f == NULL)
     {
         printf("Could not open file\n");
         return 1;
@@ -24,15 +24,16 @@ int main(int argc, char *argv[])
     char namefile[8];
     FILE *file = NULL;
     // Reading through the 512 Bytes of the Memory Card File
-    fread(buffer, sizeof(unsigned char), 512, f);
-        sprintf(namefile, "%03i.jpg", file_counter);
+    while (fread(buffer, sizeof(unsigned char), 512, f) == 512)
+    {
         // Checking whether it's the start of a new jpeg
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
         {
             // Checking whether it's the first jpeg
-            if(Firstjpeg == false)
+            if (first_jpeg == false)
             {
-                Firstjpeg = true;
+                first_jpeg = true;
             }
             // Checking whether it's not the first jpeg
             else
@@ -41,10 +42,10 @@ int main(int argc, char *argv[])
             }
         }
         // Checking whether it's not the start of a new jpeg
-            if(Firstjpeg == true)
-            {
-                fwrite(buffer, sizeof(unsigned char), 512, file);
-            }
+        sprintf(namefile, "%03i.jpg", file_counter);
+        if (first_jpeg == true)
+        {
+            fwrite(buffer, sizeof(unsigned char), 512, file);
         }
     }
     return (false);
